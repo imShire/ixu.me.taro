093b632d8f5d09f58e9af6c7bcb9f1c7ee7fcf5c
JaneCC <i@janecc.com>
2019-05-16 14:09:35 +0800

    Update ecode.ts

--- src/constants/ecode.ts
+++ src/constants/ecode.ts
@@ -1,15 +1,26 @@
 export const HTTP_STATUS = {
-  // 成功
-  SUCCESS: 0,
-  // 错误
-  ERROR: 0,
-  // token 失效
-  AUTHENTICATE: 80001,
-  CLIENT_ERROR: 400,
-  FORBIDDEN: 403,
-  NOT_FOUND: 404,
-  SERVER_ERROR: 500,
-  BAD_GATEWAY: 502,
-  SERVICE_UNAVAILABLE: 503,
-  GATEWAY_TIMEOUT: 504
-}
+    // 成功
+    SUCCESS: 0,
+    // 错误
+    ERROR: 1,
+    // token 失效
+    AUTHENTICATE: 80001,
+    // 请求错误
+    CLIENT_ERROR: 400,
+    // 未授权，请重新登录
+    CLIENT_AUTH_ERROR: 401,
+    // 拒绝访问
+    FORBIDDEN: 403,
+    // 请求出错
+    NOT_FOUND: 404,
+    // 请求超时
+    REQUEST_TIMEDOUT: 408,
+    // 服务器错误
+    SERVER_ERROR: 500,
+    // 网络错误
+    BAD_GATEWAY: 502,
+    // 服务不可用
+    SERVICE_UNAVAILABLE: 503,
+    // 网络超时
+    GATEWAY_TIMEOUT: 504
+};
